2cb244f29b5be7a60295c1ccb18a3ef0a4e496e4
Hani Awni <hani.awni@gmail.com>
2019-07-15 08:27:40 -0400

    add rosbagging to subdirectory and example rqtgraph

--- mapper/start_mapping.sh
+++ mapper/start_mapping.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
+tmux new-session -d -s rosbagger
+tmux send-keys -t rosbagger "catsource" C-m
+tmux send-keys -t rosbagger "rosbag record -o 'bags/' /zed/zed_node/rgb/image_rect_color /zed/zed_node/rgb/camera_info /zed/zed_node/right/image_rect_color /zed/zed_node/right/camera_info /zed/zed_node/depth/camera_info /zed/zed_node/depth/depth_registered /zed/zed_node/odom /tf" C-m
+echo "Now running rosbag in tmux session rosbagger."
 tmux new-session -d -s zedmapper
-tmux send-keys -t zedmapper "catsource && :3" C-m
+tmux send-keys -t zedmapper "catsource" C-m
 tmux send-keys -t zedmapper "roslaunch zed_wrapper zed.launch camera_model:=zed" C-m
-echo "Now running Zed_Wrapper in tmux session zedmapper. \n\n Check tmux sessions via tmux attach-session -t (session name).\n List sessions via tmux ls."
+echo "Now running Zed_Wrapper in tmux session zedmapper."
+echo "\n Check tmux sessions via tmux attach-session -t (session name).\n List sessions via tmux ls."
