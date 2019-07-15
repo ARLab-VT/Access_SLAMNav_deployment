e3b96a1f0162f8adbdf5d90dee8c1aa472d530ff
Hani Awni <hani.awni@gmail.com>
2019-07-15 13:28:45 -0400

    ditch right camera data

--- mapper/start_mapping.sh
+++ mapper/start_mapping.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 tmux new-session -d -s rosbagger
 tmux send-keys -t rosbagger "catsource" C-m
-tmux send-keys -t rosbagger "rosbag record --lz4 -o 'bags/' /zed/zed_node/rgb/image_rect_color /zed/zed_node/rgb/camera_info /zed/zed_node/right/image_rect_color /zed/zed_node/right/camera_info /zed/zed_node/depth/camera_info /zed/zed_node/depth/depth_registered /zed/zed_node/odom /tf" C-m
+tmux send-keys -t rosbagger "rosbag record --lz4 -o 'bags/' /zed/zed_node/rgb/image_rect_color /zed/zed_node/rgb/camera_info /zed/zed_node/depth/camera_info /zed/zed_node/depth/depth_registered /zed/zed_node/odom /tf" C-m
 echo "Now running rosbag in tmux session rosbagger."
 tmux new-session -d -s zedmapper
 tmux send-keys -t zedmapper "catsource" C-m
