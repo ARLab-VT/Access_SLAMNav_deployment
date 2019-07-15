363a62f6040295c3c895cda272d3a35516f5c394
Hani Awni <hani.awni@gmail.com>
2019-07-15 07:50:35 -0400

    fix tmux launch command to use sendkeys

--- mapper/start_mapping.sh
+++ mapper/start_mapping.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
-tmux new-session -d -s zedmapper 'roslaunch zed_wrapper zed.launch camera_model:=zed'
-echo "Now running Zed_Wrapper in tmux session zedmapper. \n Check it via tmux attach-session -t zedmapper."
+tmux new-session -d -s zedmapper
+tmux send-keys -t zedmapper "catsource && :3" C-m
+tmux send-keys -t zedmapper "roslaunch zed_wrapper zed.launch camera_model:=zed" C-m
+echo "Now running Zed_Wrapper in tmux session zedmapper. \n\n Check tmux sessions via tmux attach-session -t (session name).\n List sessions via tmux ls."
